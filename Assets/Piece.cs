b1706e724f51c13cc289385dc0f7eb575dcdef3f
hktrollgames <hktrollgames@gmail.com>
2021-09-27 20:29:43 +0900

    클릭 시작한 위치로 드래그 되게함

--- Assets/Piece.cs
+++ Assets/Piece.cs
@@ -7,14 +7,20 @@ using UnityEngine.UI;
 public class Piece : MonoBehaviour, IDragHandler
     , IBeginDragHandler, IEndDragHandler
 {
+    public Vector2 offsetDrag;
     public void OnBeginDrag(PointerEventData eventData)
     {
+        Vector2 pos = new Vector2(transform.position.x
+            , transform.position.y);
+
+        offsetDrag = pos - eventData.position;
+
         GetComponent<Image>().raycastTarget = false;
         transform.SetAsLastSibling();
     }
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        transform.position = eventData.position + offsetDrag;
     }
 
     public void OnEndDrag(PointerEventData eventData)
