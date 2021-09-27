847be34f4c26e1c932128b3e773c3b4e5077e5d8
hktrollgames <hktrollgames@gmail.com>
2021-09-27 20:12:24 +0900

    올바른 위치 드래그하면 번쩍이고 나서 드래그 안되게 레이케스트 타겟 끔

--- Assets/FixedPiece.cs
+++ Assets/FixedPiece.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class FixedPiece : MonoBehaviour, IDropHandler
 {
@@ -13,5 +15,34 @@ public class FixedPiece : MonoBehaviour, IDropHandler
 
         // 위치 스냅.
         eventData.pointerDrag.transform.position = transform.position;
+
+        // 올바른 위치에 드랍되었는지 확인
+        if(IsRightPosition(eventData.pointerDrag))
+        {
+            //// 오바른위치에 드래그됨.
+            //번쩍이게 표시하자.
+            var dragImage = eventData.pointerDrag.GetComponent<Image>();
+            StartCoroutine(BlinkAndDisableClickCo(dragImage, 0.1f));
+
+            ////점수 증가 시키자 - 100점 올리기
+            //GameManager.Instance.AddScore(100);
+        }
+    }
+
+    private IEnumerator BlinkAndDisableClickCo(Image dragImage, float time)
+    {
+        dragImage.color = Color.red;
+        yield return new WaitForSeconds(time);
+        dragImage.color = Color.white;
+
+        // 정상위치에 드래그 된건 움직이지 못하게 하자.
+        dragImage.raycastTarget = false;
+    }
+
+    private bool IsRightPosition(GameObject dragObject)
+    {
+        // dragObject.name -> "3:2 (1)"
+        var dragObjectName = dragObject.name.Replace(" (1)", "");
+        return name.CompareTo(dragObjectName) == 0;
     }
 }
