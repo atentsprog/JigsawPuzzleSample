0995909e6bcc681b16506f0a1939e329379476f2
hktrollgames <hktrollgames@gmail.com>
2021-09-27 19:09:55 +0900

    피스 위치 시킴

--- Assets/PuzzlePiecePosInit.cs
+++ Assets/PuzzlePiecePosInit.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 화면 중앙에 표시하자.
+/// 피스를 드래그 시키자.
+/// </summary>
 public class PuzzlePiecePosInit : MonoBehaviour
 {
     public List<Sprite> sprites;
@@ -23,19 +27,21 @@ public class PuzzlePiecePosInit : MonoBehaviour
         float pieceHeight = sprites[0].textureRect.height;
 
         int imageIndex = 0;
-        for (int y = 0; y < yCount; y++)
+        for (int y = 1; y <= yCount; y++)
         {
-            for (int x = 0; x < xCount; x++)
+            for (int x = 1; x <= xCount; x++)
             {
                 var item = new GameObject($"{x}:{y}");
                 item.transform.parent = transform;
+                item.transform.localScale = Vector3.one;
                 RectTransform rt = item.AddComponent<RectTransform>();
                 // 크기
                 rt.sizeDelta = new Vector2(pieceWidth, pieceHeight);
 
                 // 위치.
-                float xPos = x * pieceWidth;
-                float yPos = -y * pieceHeight;
+                float xPos = (x - xCount) * (pieceWidth * 0.5f) + (x - 1) * (pieceWidth * 0.5f);
+                float yPos = -(y - yCount) * (pieceHeight * 0.5f) + -(y - 1) * (pieceHeight * 0.5f);
+
                 item.transform.localPosition = new Vector3(xPos, yPos, 0);
 
                 item.AddComponent<Image>().sprite = sprites[imageIndex];
@@ -48,6 +54,11 @@ public class PuzzlePiecePosInit : MonoBehaviour
     {
         var childs = transform.GetComponentsInChildren<Image>();
         foreach (var item in childs)
-            DestroyImmediate(item.gameObject);
+        {
+            if(Application.isPlaying)
+                Destroy(item.gameObject);
+            else
+                DestroyImmediate(item.gameObject);
+        }
     }
 }
